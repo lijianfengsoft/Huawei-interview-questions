b8be16f37d50a749d8d90fb8127da55ab97034f4
lijianfengsoft <li_jian__feng@163.com>
2018-03-30 11:26:39 +0800

    first commit

---
+++ Project1/Project1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <string.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int n, i=0;
+	int num = 0;
+	char c;
+	char String[100];
+	char *p = String;
+
+	gets(String);
+	n=strlen(String);
+	printf("%d\r\n", n);
+	for (i = n; i > 0; i--)
+	{
+		c = *(p + i-1);
+		if (c != ' ')
+		{
+			printf("%c\r\n", c);
+			num++;
+		}
+		else
+			break;
+	}
+
+	printf("%d\r\n", num);
+
+	system("pause");
+	return 0;
+}
